839fba1d1167f4f4e8eef6b02459a3e8405c4ae5
saqib ali <saqib@nclouds.com>
2016-11-29 18:53:55 +0000

    added motd and changed apache default attributes

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,8 +4,15 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 #
+
+if node['platform_family'] == "rhel"
+	package = "httpd"
+elseif node['platform_famliy'] == "debian"
+	package = "apache2"
+end
+
 package 'apache2' do
-	package_name 'httpd'
+	package_name package
 	action :install
 end
 
@@ -14,4 +21,5 @@ service 'apache2' do
 	action [:start, :enable]
 end
 
+#include_recipe 'apache::wesites'
 
